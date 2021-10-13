549aa5f7552422130aa9c8a51def04b749991b69
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2021-10-13 11:15:07 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public interface DepartmentDao {
+	
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	
+	Seller findById(Integer id);
+	List<Seller> findAll();
+}
