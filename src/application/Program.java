82edc80c2c51acbca5f3f2308dbad7a5399b44b4
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2021-10-13 17:38:13 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,8 +10,8 @@ public class Program {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(7);
-		
 		System.out.println(seller);
 	}
 } 
